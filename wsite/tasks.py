16d6db5a099d1c467ff1824e18c0911c84e97ce7
dauntless <suntopmultimedia@gmail.com>
2023-01-08 17:34:41 +0100

    celery, template & ref

--- wsite/tasks.py
+++ wsite/tasks.py
@@ -1,38 +1,37 @@
-import os
 from celery import shared_task
-
 from django.utils.timezone import now, timedelta, datetime
-
 from sdtmonitor.celery import app
 from django.core.mail import send_mail
 from django.conf import settings
 from django.core import mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-
-from wsite.models import Website, WebsiteEmail
+from wsite.models import Website
+from sdtmonitor.utils.func import get_website_status
 
 
 def send_email(context, website, to):
     from_email = settings.EMAIL_HOST_USER
     subject = f'Website Update for {website}'
-    html_message = render_to_string('base/daily_overview.html',context)
+    html_message = render_to_string('report_notification.html',context)
     plain_message = strip_tags(html_message)
     mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)    
 
 @app.task()
-def send_report_overview():
-    environment = os.getenv("ENVIRONMENT", "DEV")
-    url = None
-
-    # if environment == 'DEV':
-    #     url = "http://127.0.0.1:8000"
-    url = "http://127.0.0.1:8000"
+def send_report_notification():
     websites = Website.objects.all()
-    # for website in websites:
-    #     context = {
-    #     'url' : url
-    #     }
-    #     send_email(context=context, to=[], website=website)
-                    
+    for website in websites:
+        status = get_website_status(website.url)
+        if status != website.status:
+            last_time = website.modified_at
+            website.status = status
+            website.save()
+            duration = website.modified_at - last_time 
+            msg = 'Your site is down'
+            if status == 'up':
+                msg = f'Your site is now up and running. Downtime duration is {duration}'
+            context = {
+                'message' : msg,
+            }
+            send_email(context=context, to=[email.email for email in website.websiteemail_set.all()], website=website)             
     return True
