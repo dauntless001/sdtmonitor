a368990a61d1d9a3a6363f8b3654aaf786d307fd
NikkyXO <alexisdame2017@gmail.com>
2023-01-11 20:17:16 +0100

    Updated Settings

--- sdtmonitor/utils/settings.py
+++ sdtmonitor/utils/settings.py
@@ -1,10 +1,10 @@
 import os
-
+from decouple import config
 def get_app_settings():
     """
     set default settings module or get from environment variable
     """
-    setting = os.getenv(
-        "DJANGO_SETTINGS_MODULE", "sdtmonitor.settings.dev"
+    setting = config(
+        "DJANGO_SETTINGS_MODULE"
     )
     return setting
